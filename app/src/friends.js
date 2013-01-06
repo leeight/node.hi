ad17923497a3b46c3e1e478d97c25fc0089f4a00
leeight <leeight@gmail.com>
2013-01-06 23:16:00 +0800

    简单的更新一下UI的资源管理方案
    尝试了gcl和requirejs, 最终选择了requirejs, 因为packaged app
    不允许document.write, 所以抛弃了gcl的方案

---
+++ app/src/friends.js
@@ -0,0 +1,138 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2013 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * assets/js/friends.js ~ 2013/01/06 21:39:06
+ * @author leeight(liyubei@baidu.com)
+ * @version $Revision$ 
+ * @description 
+ *  
+ **/
+
+define(function(){
+
+/**
+ * key => imid
+ * value => {lib.model.Friend}
+ * @type {Object.<string, Object>}
+ */
+var _friends = {};
+
+/**
+ * 添加好友的信息.
+ * @param {Object} friend 好友.
+ */
+function add(friend) {
+  if (friend['imid']) {
+    _friends[friend['imid']] = friend;
+  }
+}
+
+/**
+ * TODO 支持拼音?
+ * 按照关键词搜索, 返回一批好友
+ * @param {string} query 搜索关键词.
+ * @param {number=} opt_max 最多返回多少个?
+ * @return {Array.<Object>}
+ */
+function search(query, opt_max) {
+  var max = opt_max || 20;
+  var friends = [];
+
+  for(var key in _friends) {
+    if (_friends.hasOwnProperty(key)) {
+      var friend = _friends[key];
+      var weight = _isMatch(friend, query);
+      if (weight > 0) {
+        friends.push([weight, friend]);
+        if (friends.length >= max) {
+          break;
+        }
+      }
+    }
+  }
+
+  return friends.sort(function(a, b){
+    return b[0] - a[0];
+  }).map(function(a){
+    return a[1];
+  });
+}
+
+/**
+ * 按照一定规则展示某些好友.
+ * 因为没有必要展示全部的, 如果想跟某个人聊天, 去搜索就好了.
+ * @return {Array.<Object>}
+ */
+function getTopFriends() {
+  var friends = [];
+  for(var imid in _friends) {
+    if (_friends.hasOwnProperty(imid)) {
+      var friend = _friends[imid];
+      if (friend['status'] <= 3) {
+        friends.push(friend);
+      }
+    }
+  }
+  return friends;
+}
+
+/**
+ * @param {Object} friend 好友信息.
+ * @param {string} query 关键词.
+ * @return {number} 0没有找到, 否则返回符合的权重用来呈现UI
+ * 的时候进行排序.
+ */
+function _isMatch(friend, query) {
+  if (!friend) {
+    return 0;
+  }
+
+  var keyword = query.toLowerCase();
+  var weight = 0;
+  var attributes = [
+    'baiduid',
+    'personal_comment',
+    'name',
+    'nickname',
+    'new_username'
+  ];
+  var size = attributes.length;
+
+  for(var i = 0, key = null; key = attributes[i++]; ) {
+    if (friend[key] && (friend[key].toLowerCase()).indexOf(keyword) > -1) {
+      weight += (size - i ) * (size - i);
+    }
+  }
+
+  return weight;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return {
+  add: add,
+  search: search,
+  getTopFriends: getTopFriends
+}
+
+
+});
+/* vim: set ts=4 sw=4 sts=4 tw=100: */
---
+++ app/src/ui/login.js
@@ -0,0 +1,149 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2013 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * assets/js/login.js ~ 2013/01/06 15:38:15
+ * @author leeight(liyubei@baidu.com)
+ * @version $Revision$ 
+ * @description 
+ *  
+ **/
+require(['../friends'], function(FRIENDS){
+$(function(){
+  var IS_CONNECTED = false;
+  var tpl_contact = $("#TPL-contact").val();
+
+  function on_relogin() {
+    IS_CONNECTED = false;
+    $(".contacts").hide("slow").html('<ul/>');
+    $("form").show("slow");
+  }
+
+  function on_connect() {
+    console.log("connect");
+    IS_CONNECTED = true;
+  }
+
+  // 登陆成功, 展现联系人的界面
+  function on_login_success() {
+    $("form").hide("fadeIn");
+    $(".contacts").show("fadeOut");
+  }
+
+  // 获取到联系人的信息, 展示联系人
+  function on_friend_list(friends) {
+    var html = [];
+    friends.forEach(function(friend){
+      FRIENDS.add(friend);
+      html.push(Mustache.to_html(tpl_contact, friend));
+    });
+    $(".contacts ul").append(html.join(''));
+  }
+
+  // 展示过滤之后的结果.
+  function display_filter_result(friends) {
+    $(".contacts ul").empty();
+
+    if (!friends || friends.length <= 0) {
+      $(".contacts ul").append('<li class="empty">Not found any matched contact.</li>');
+      return;
+    }
+
+    var html = [];
+    friends.forEach(function(friend){
+      html.push(Mustache.to_html(tpl_contact, friend));
+    });
+    $(".contacts ul").append(html.join(''));
+  }
+
+  // 获取到新的消息.
+  function on_new_message(message) {
+    console.log(message);
+  }
+
+  var socket = io.connect('http://localhost:8888');
+  socket.on('connect', on_connect);
+  socket.on('disconnect', on_relogin);
+  socket.on('close', on_relogin);
+  socket.on('login_success', on_login_success);
+  socket.on('friend_list', on_friend_list);
+  socket.on('new_message', on_new_message);
+
+  // --- 页面UI事件绑定 ---
+  $("button[type=submit]").click(function(){
+    if (!IS_CONNECTED) {
+      return false;
+    }
+
+    var username = $("#username").val().trim();
+    var password = $("#password").val().trim();
+
+    if (!username || !password) {
+      return false;
+    }
+
+    socket.emit('login', {
+      'username': username,
+      'password': password
+    });
+    return false;
+  });
+
+  $(".contacts").on("click", "li", function(){
+    console.log(this);
+    return false;
+  });
+
+  $(".filter input").on('input', function(){
+    var query = $(this).val().trim();
+    if (query.length > 3) {
+      display_filter_result(FRIENDS.search(query));
+    } else if (query.length <= 0) {
+      // 重新展示所有的好友
+      on_friend_list(FRIENDS.getTopFriends());
+    }
+    console.log(this);
+  });
+
+  // --- DEBUG CODE ---
+(function(){
+  on_login_success();
+  var friends = [];
+  for(var i = 0; i < 100; i ++) {
+    friends.push({
+      imid: i,
+      status: [1,2,3,4,5][parseInt(Math.random() * 5, 10)],
+      baiduid: 'account' + i,
+      personal_comment: (Math.random() > 0.5) ? ('personal_comment' + i) : null,
+    });
+  }
+  on_friend_list(friends);
+})();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+/* vim: set ts=4 sw=4 sts=4 tw=100: */
