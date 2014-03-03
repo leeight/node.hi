883cc4666d36ba1e2542314db4c46a3e83186d39
leeight <leeight@gmail.com>
2014-03-03 13:16:39 +0800

    Add index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * index.js ~ 2014/03/03 13:16:27
+ * @author leeight(liyubei@baidu.com)
+ * @version $Revision$ 
+ * @description 
+ * hello index.js
+ **/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100: */
